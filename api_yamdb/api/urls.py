411c25ce86d9d2cbb7e8e8d6f2fb64adc812d94e
Стас Басов <stas.basov@whatever.com>
2023-02-16 20:55:17 +0300

    feature/permitions_v1

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 
 from .views import (APIGetToken, APISignup, UsersViewSet)
 
 app_name = 'api'
 
-router = SimpleRouter()
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register(
     'users',
     UsersViewSet,
     basename='users'
@@ -14,6 +14,6 @@ router.register(
 
 urlpatterns = [
     path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/', APISignup.as_view(), name='signup'),
 ]
