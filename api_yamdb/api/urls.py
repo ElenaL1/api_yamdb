15ab789ed8172441ac100628d3717e3736acb413
Елена Ламберт <elena_lambert@yahoo.com>
2023-02-16 17:08:29 +0300

    Minor changes in order to runserver

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import CommentViewSet
+from .views import (CategoryViewSet, CommentViewSet,
+                    GenreViewSet, TitleViewSet)
+
+app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
+router_v1.register('titles', TitleViewSet, basename='titles')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register(
     r'reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
-
 
 urlpatterns = [
     path('v1/', include(router_v1.urls))
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -132,3 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 EMAIL_ADMIN = 'Admin@admin.com'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
