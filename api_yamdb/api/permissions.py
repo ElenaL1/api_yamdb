bc0a91f7378fcb102c082d6cfc9e6d536d56a806
virusfx <virusfx@yandex.ru>
2023-02-17 14:19:50 +0800

    Исправление permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,17 +1,21 @@
-from rest_framework.permissions import BasePermission
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class AdminOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_staff
 
 
 class IsAdminOrReadOnly(BasePermission):
     message = 'Доступ только у администратора.'
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method in SAFE_METHODS:
             return True
         return request.user.is_staff
 
 
 class IsAuthorPermission(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
+        return (request.method in SAFE_METHODS
                 or obj.author == request.user)
