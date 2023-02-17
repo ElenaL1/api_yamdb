e644bc1015bc0bc77eb42993ce9bdc3566f1ef4e
virusfx <virusfx@yandex.ru>
2023-02-17 14:38:14 +0800

    Исправление Urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from .views import (CategoryViewSet, CommentViewSet,
-                    GenreViewSet, TitleViewSet)
+from .views import (APIGetToken, APISignup, UsersViewSet, ReviewViewSet,
+                    CommentViewSet, CategoryViewSet, TitleViewSet,
+                    GenreViewSet)
 
 app_name = 'api'
 
-router_v1 = routers.DefaultRouter()
+router_v1 = DefaultRouter()
+router_v1.register('reviews', ReviewViewSet, basename='reviews')
 router_v1.register('titles', TitleViewSet, basename='titles')
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
@@ -15,7 +17,30 @@ router_v1.register(
     CommentViewSet,
     basename='comments'
 )
+router_v1 = DefaultRouter()
+router_v1.register(
+    'users',
+    UsersViewSet,
+    basename='users'
+)
+router_v1.register(
+    'categories',
+    CategoryViewSet,
+    basename='сategories'
+)
+router_v1.register(
+    'titles',
+    TitleViewSet,
+    basename='titles'
+)
+router_v1.register(
+    'genres',
+    GenreViewSet,
+    basename='genres'
+)
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls))
+    path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
+    path('v1/', include(router_v1.urls)),
+    path('v1/auth/signup/', APISignup.as_view(), name='signup'),
 ]
