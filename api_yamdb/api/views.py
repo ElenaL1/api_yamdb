df689a8a122a66ae39ab714f87823e54164d62db
ElenaL1 <115463400+ElenaL1@users.noreply.github.com>
2023-02-26 22:38:12 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -46,7 +46,7 @@ class GenreViewSet(CreateListDestroyViewSet):
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all().annotate(Avg("reviews__score")
-                                           ).order_by("name")
+                                            ).order_by("name")
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
     filterset_class = TitlesFilter
